37e26297a7168f52660a8004dc60840d5d40ad5e
Wagner Narde <5348+wagner@users.noreply.github.com>
2021-11-23 14:52:06 -0300

    Primeira versão (#3)
    * Corrige path das imagens
    
    * Remove tema
    
    * Correções
    
    * Adiciona imagem about
    
    * Cria layout para artigo
    
    * Adiciona header e footer
    
    * Corrige imagem do about
    
    * Adiciona links
    
    * Remove seta girando
    
    * Adiciona .ruby-version
    
    * Esconde botões superiores
    
    * About como página principal
    
    * Remove imagens de /assets
    
    * Modifica imagens no about
    
    * Atualiza background
    
    * Esconde botões
    
    * Corrige foto
    
    * Adiciona sublinado no link
    
    * Correção no conteúdo

---
+++ assets/js/banner/create.js
@@ -0,0 +1,46 @@
+import bannerLayers from './layers.js'
+import movement from './movement.js'
+import change from './change.js'
+import touch from './touch.js'
+import {allLayers, allLayersMobile} from './config.js'
+const create = {
+	allL: allLayers,
+	init: function () {
+		document.querySelector('.tright').addEventListener('click', change.changeBanner)
+		document.querySelector('.tleft').addEventListener('click', change.changeBanner)
+
+		if (window.screen.width < 800) {
+			create.allL = allLayersMobile
+			document.body.ontouchstart = touch.start
+			document.body.ontouchmove = touch.move
+			document.body.ontouchend = touch.leave
+		}
+		const bannerContent = document.querySelector('.banner-content')
+		bannerContent.style.width = '200%'
+		var banner = create.one(0, false)
+		bannerContent.append(banner)
+		document.body.className = `banner0`
+		movement.init()
+	},
+	one: function (pos, isPrevious) {
+		if (pos < 0) {
+			pos = allLayers.length - 1
+		}
+		if (pos > allLayers.length - 1) {
+			pos = 0
+		}
+
+		var banner = bannerLayers.init(create.allL[pos], pos)
+		banner.id = `banner${pos}`
+		banner.style.width = (isPrevious) ? '0%' : '50%'
+		banner.style.zIndex = 1
+		banner.style.position = 'relative'
+		banner.dataset.pos = pos
+		banner.style.marginTop = '0px'
+		banner.style.marginRight = '0px'
+		banner.style.marginLeft = '0px'
+		banner.style.marginBottom = '0px'
+		return banner
+	}
+}
+export default create
---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+import create from './banner/create.js'
+import colorScreen from './colorScreen.js'
+import navBar from './nav.js'
+import fakeJson from './fake-ajax.js'
+
+window.onload = function () {
+	navBar.init()
+	colorScreen.setup()
+	fakeJson.init()
+
+	if (document.querySelector('.banner-content')) {
+		create.init()
+	}
+} 
